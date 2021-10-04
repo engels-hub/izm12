414854d277734477f6208dc781850410b0263013
engels-hub <artjompapsh2003@gmail.com>
2021-10-05 00:26:42 +0300

    teacher view done

--- src/app/components/teacher-table-mobile/teacher-table-mobile.component.ts
+++ src/app/components/teacher-table-mobile/teacher-table-mobile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-teacher-table-mobile',
@@ -9,7 +9,70 @@ export class TeacherTableMobileComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  @Input() schedule!: any; //from parent
+  columns = new Array();
+  search: any;
+  filtered: any;
+  loaded = false;
+
+  ngOnInit():void{
+    this.columns[0] = new Array();
+    this.columns[1] = new Array();
+  }
+  ngOnChanges(): void {
+    if (this.schedule && !this.loaded) {
+      this.loaded = true;
+      this.filtered = this.schedule.data.teachers;
+    }
+
+  }
+
+  onFilterChange(query: any, time: number) {
+    let target = query.target.value.toLowerCase();
+
+    target.split(" ").forEach((element: String) => {
+
+      this.filtered = [];
+      let all = this.schedule.data.teachers;
+      for (let i = 0; i < all.length; i++) {
+        let sum = all[i].teacherName.replace(' ', '');
+        for (let j = 0; j < all[i].lessons.length; j++) {
+          sum+=all[i].lessons[j].lessonNumber+".";
+          sum+=all[i].lessons[j].class;
+          sum+=this.getClassName(all[i].lessons[j].class);
+          sum+=all[i].lessons[j].room+".";
+          sum+=this.tryNote(all[i].lessons[j].note);
+        }
+        sum = sum.toLowerCase();
+
+        if (sum.includes(element)) {
+          this.filtered.push(all[i]);
+        }
+      }
+
+
+    });
+  }
+
+
+  public tryNote(note: any) {
+    try {
+      return note.noteText
+    } catch (error) { //room.note or room.note.noteText might be null
+      return ''
+    }
+  }
+
+  public getClassName(className: any) { //13a -> 13.a
+    let number = className.replace(/[a-zA-Z]/i, '');
+    let letter = className.replace(number, '');
+    return number + "." + letter;
+  }
+
+  public emptySearch(){
+    this.search='';
+
+    this.filtered=this.schedule.data.teachers;
   }
 
 }
--- src/app/components/teacher-table/teacher-table.component.ts
+++ src/app/components/teacher-table/teacher-table.component.ts
@@ -9,9 +9,80 @@ export class TeacherTableComponent implements OnInit {
 
   constructor() { }
 
-  @Input() data?: any;
+  @Input() schedule!: any; //from parent
+  columns = new Array();
+  search: any;
+  filtered: any;
+  loaded = false;
 
-  ngOnInit(): void {
+  ngOnInit():void{
+    this.columns[0] = new Array();
+    this.columns[1] = new Array();
+  }
+  ngOnChanges(): void {
+    if (this.schedule && !this.loaded) {
+      this.loaded = true;
+      this.filtered = this.schedule.data.teachers;
+      this.columnMaker(this.filtered)
+    }
+
+  }
+
+  onFilterChange(query: any, time: number) {
+    let target = query.target.value.toLowerCase();
+
+    target.split(" ").forEach((element: String) => {
+
+      this.filtered = [];
+      let all = this.schedule.data.teachers;
+      for (let i = 0; i < all.length; i++) {
+        let sum = all[i].teacherName.replace(' ', '');
+        for (let j = 0; j < all[i].lessons.length; j++) {
+          sum+=all[i].lessons[j].lessonNumber+".";
+          sum+=all[i].lessons[j].class;
+          sum+=this.getClassName(all[i].lessons[j].class);
+          sum+=all[i].lessons[j].room+".";
+          sum+=this.tryNote(all[i].lessons[j].note);
+        }
+        sum = sum.toLowerCase();
+
+        if (sum.includes(element)) {
+          this.filtered.push(all[i]);
+        }
+      }
+
+      this.columnMaker(this.filtered)
+
+    });
+  }
+
+  public columnMaker(data: any){
+    this.columns[0]=[]
+    this.columns[1]=[]
+
+    for (let i = 0; i < data.length; i++) //divide data into two columns
+    {
+      this.columns[i % 2].push(data[i])
+    }
+  }
+
+  public tryNote(note: any) {
+    try {
+      return note.noteText
+    } catch (error) { //room.note or room.note.noteText might be null
+      return ''
+    }
+  }
+
+  public getClassName(className: any) { //13a -> 13.a
+    let number = className.replace(/[a-zA-Z]/i, '');
+    let letter = className.replace(number, '');
+    return number + "." + letter;
+  }
+
+  public emptySearch(){
+    this.search='';
+    this.columnMaker(this.schedule.data.teachers);
   }
 
 }
